6f2cb2805b7a65aca741ee0daec9c41d90710acd
Ole Petter Skovseth <SoapyOpey@users.noreply.github.com>
2015-02-15 15:30:18 +0100

    Update Map.java

--- app/src/main/java/no/gruppe2/shera/Map.java
+++ app/src/main/java/no/gruppe2/shera/Map.java
@@ -5,12 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-<<<<<<< HEAD
-
 import com.facebook.Session;
-=======
->>>>>>> master
-
 
 public class Map extends ActionBarActivity {
 
